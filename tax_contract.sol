b4a879827775ffcabfb31dfa6725d3fd69abf339
thiru1003 <141509276+thiru1003@users.noreply.github.com>
2024-04-21 02:58:18 -0700

    Update tax_contract.sol

--- tax_contract.sol
+++ tax_contract.sol
@@ -1,25 +1,29 @@
 // SPDX-License-Identifier: GPL-3.0
-
 pragma solidity ^0.8.1;
-
 contract SendMoneyExample {
-
+    address public owner;
+    bool public paused;
+    constructor() {
+        owner = msg.sender;
+    }
     uint public balanceReceived;
-
     function receiveMoney() public payable {
         balanceReceived += msg.value;
     }
-
     function getBalance() public view returns(uint) {
         return address(this).balance;
     }
-
     function withdrawMoney() public {
         address payable to = payable(msg.sender);
         to.transfer(getBalance());
     }
-
+    function setPaused(bool _paused) public {
+        require(msg.sender == owner, "You are not the owner");
+        paused = _paused;
+    }
     function withdrawMoneyTo(address payable _to) public {
+        require(owner == msg.sender, "You cannot withdraw.");
+        require(paused == false, "Contract Paused");
         _to.transfer(getBalance());
     }
 }
